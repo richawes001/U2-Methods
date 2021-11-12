66fd6ef6991ac0f1055a509088e4dcd8a5fc2d88
richawes001 <richawes001@warren.k12.in.us>
2021-11-12 11:39:53 -0500

    Recompleted Artwork GUI and Artwork

--- L7-Swing Colors and Paint/src/ArtworkGUI.java
+++ L7-Swing Colors and Paint/src/ArtworkGUI.java
@@ -6,6 +6,12 @@ public class ArtworkGUI {
     //DECLARE VARIABLES
 
     JFrame frame;
+    Color colorOne = new Color(255, 187, 187);
+    Color colorTwo = new Color(255, 28, 28);
+    Color colorThree = new Color(147, 69, 69);
+    Color colorFour = new Color(110, 94, 94);
+    Color colorFive = new Color(155, 57, 57);
+    Color colorSix = new Color(180, 138, 138);
 
     public ArtworkGUI() {
         //CREATE THE GUI
@@ -27,7 +33,7 @@ public class ArtworkGUI {
         public MyDrawing(){
             //Set the background color
 
-            setBackground(Color.cyan);
+            setBackground(colorSix);
 
         }
 
@@ -37,21 +43,21 @@ public class ArtworkGUI {
 
             super.paintComponent(g);
 
-            g.setColor(Color.red);
+            g.setColor(colorOne);
             g.drawLine(200, 225, 300, 375);
             g.drawLine(200,225, 100, 375);
             g.drawLine(100, 375, 300, 375);
 
-            g.setColor(Color.black);
+            g.setColor(colorTwo);
             g.fillRect(100, 375, 200, 225);
 
-            g.setColor(Color.darkGray);
+            g.setColor(colorThree);
             g.fillRect(125, 400, 150, 175);
 
-            g.setColor(Color.lightGray);
+            g.setColor(colorFour);
             g.fillRect(150, 425, 100, 125);
 
-            g.setColor(Color.white);
+            g.setColor(colorFive);
             g.fillRect(175, 450, 50, 75);
 
 
